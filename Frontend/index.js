3bad6ffbf46179dc5c9bed3c601c247a26b9ebcb
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-09-21 18:09:14 -0400

    done

--- Frontend/index.js
+++ Frontend/index.js
@@ -12,11 +12,10 @@ const formContainer = document.querySelector("div#form-container")
 
 
 
-
-
 fetch(url)
 .then(r => r.json())
 .then(shoesObj => {
+    firstShoe(shoesObj)
     shoesObj.forEach(shoeObj => {
         turnObjtoHTML(shoeObj)
     })
@@ -36,57 +35,36 @@ function turnObjtoHTML(shoeObj){
         fetch(url+`/${shoeObj.id}`) 
         .then(r => r.json())
         .then(currentObj => {
+            //single shoe
             shoeIMG.src = currentObj.image
             shoeTitle.innerText = currentObj.name
             shoeDescription.innerText = currentObj.description
             shoePrice.innerText = "$" + currentObj.price
 
-            //reviews list 
-            let reviewsArr = currentObj.reviews
-            reviewsArr.forEach(review => {
-                logReview(review)
-            })
+            addingReview(currentObj)
 
-            function logReview(review){
-                let reviewLI = document.createElement("li")
-                reviewLI.innerText = review.content
-                // reviewLI.setAttribute("style", "list-style-type:none;" )
-                reviewsUL.append(reviewLI)
-            }
-        
-            //formBox on top of Review, using a fetch patch
-            let formBox = document.createElement('form')
-            
-            formContainer.append(formBox) 
-
-            currentObj.reviews.push(userInput)
-            fetch(url+ `/${currentObj.id}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    review: currentObj.reviews
-                })
-            })
-
-
-
+            reviewForm(currentObj)
 
         })
     })
 
-
-
-
-
 }   
 
 
-
-
-
 //_________________________________________________
+
+function firstShoe(shoeArr) {
+    let firstShoe = shoeArr[0]
+
+    shoeIMG.src = firstShoe.image
+    shoeTitle.innerText = firstShoe.name
+    shoeDescription.innerText = firstShoe.description
+    shoePrice.innerText = "$" + firstShoe.price
+
+    addingReview(firstShoe)
+    reviewForm(firstShoe)
+}
+
 
 function changeColor(ele){
     ele.addEventListener("mouseenter", (evt)=> {
@@ -95,4 +73,66 @@ function changeColor(ele){
     ele.addEventListener("mouseleave", (evt)=> {
         evt.target.style.color = "black"
     })
+}
+
+
+function addingReview(shoe){
+    reviewsUL.innerText = ""
+    let reviewsArr = shoe.reviews
+    reviewsArr.forEach(review => {
+        addNewReview(review)
+    })
+}
+
+function addNewReview(review){
+    let reviewLI = document.createElement("li")
+    reviewLI.innerText = review.content
+    reviewsUL.append(reviewLI)
+}
+
+function reviewForm(shoe){
+    while (formContainer.firstChild) {
+        formContainer.firstChild.remove()
+      }
+    
+      let formHTML = document.createElement("form")
+          formHTML.id = "new-review"
+    
+      let formDiv = document.createElement("div")
+          formDiv.className = "form-group"
+    
+      let formTextArea = document.createElement("textarea")
+          formTextArea.className = "form-control"
+          formTextArea.id = "review-content"
+          formTextArea.rows = "3"
+    
+      let formInput = document.createElement("input")
+          formInput.className = "btn-primary"
+          formInput.type = "submit"
+    
+      formDiv.append(formTextArea, formInput)
+      formHTML.append(formDiv)
+      formContainer.append(formHTML)
+
+      
+      formHTML.addEventListener("submit", (evt) => {
+        evt.preventDefault()
+        let reviewContent = formTextArea.value
+        fetch(url+`/${shoe.id}/reviews`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              content: reviewContent
+            })
+        })
+        .then(r => r.json())
+        .then(updateReview => {
+            shoe.reviews.push(updateReview)
+            addNewReview(updateReview)
+            formHTML.reset()
+        })
+      })
+
 }
